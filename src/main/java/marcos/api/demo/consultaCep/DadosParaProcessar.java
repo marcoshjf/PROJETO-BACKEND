c01e265b636510d147dff0712a17cb1d02bf609e
marcoshjf <145942375+marcoshjf@users.noreply.github.com>
2024-04-21 17:42:12 -0300

    Add files via upload

---
+++ src/main/java/marcos/api/demo/consultaCep/DadosParaProcessar.java
@@ -0,0 +1,22 @@
+package marcos.api.demo.consultaCep;
+
+public class DadosParaProcessar {
+    private String nome;
+    private int idade;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
---
+++ src/test/java/marcos/api/demo/DemoMarcosJeronimoApplicationTests.java
@@ -0,0 +1,13 @@
+package marcos.api.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMarcosJeronimoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
